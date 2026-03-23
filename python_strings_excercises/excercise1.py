00a40128c4c9ddbfd3ddf0c82371e3df799b667b
Govind Kumar Mishra <govindkm.sv@gmail.com>
2026-03-23 14:51:19 +0530

    initial commit

---
+++ python_strings_excercises/excercise1.py
@@ -0,0 +1,18 @@
+# Write a Python program that uses three variables. The variables in your program will be animal,
+# vegetable, and mineral. Assign a string value to each one of the variables. Your program should
+# display "Here is an animal, a vegetable, and a mineral." Next, display the value for animal,
+# followed by vegetable, and finally mineral. Each one of the values should be printed on their
+# own line. Your program will display four lines in total.
+
+animal = "cat"
+vegetable = "broccoli"
+mineral = "gold"
+
+print(f"Here is an animal, a vegetable, and a mineral.\n{animal}\n{vegetable}\n{mineral}")
+
+
+# Write a Python program that prompts the user for input and simply repeats what the user
+# entered.
+
+prompt = input("Please enter something: ")
+print(f"You entered: {prompt}")
